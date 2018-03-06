ea722fbc09d7e91737828aa3ad51b0ae87c2a19d
Bogdan <bogdanivanenko@gmail.com>
2018-03-06 10:15:30 -0500

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=elibot-Web-Deploy.pubxml -p:Password=bdGvce77fpf0Z1gLYg5bcZZ3negimqbavclbY2gvGGe6L8cohXQ2suC2uG4C
+
